e77f2ebcf7a42887478cf19b9172ac37a45bb4d3
Your Name <Prithwi Das>
2023-08-05 02:59:13 +0530

    women safety

---
+++ app/src/main/java/com/womensafety/app/ChangingRoom.java
@@ -0,0 +1,14 @@
+package com.womensafety.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangingRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_changing_room );
+    }
+}
---
+++ app/src/main/java/com/womensafety/app/NewsResults.java
@@ -0,0 +1,24 @@
+package com.womensafety.app;
+
+import java.util.List;
+
+public class NewsResults {
+
+    public NewsResults(){
+
+    }
+    private List<News> articles = null;
+
+    public NewsResults(List<News> articles) {
+        this.articles = articles;
+    }
+
+    public List<News> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<News> articles) {
+        this.articles = articles;
+    }
+
+}
